baf3e9b8021dd37678de420a8068f1a379e1373a
GrentsAA <127672810+hudwizzx@users.noreply.github.com>
2024-04-30 16:31:12 +0700

    Add files via upload

---
+++ MakarovLab1/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ProcessFile_Click(object sender, RoutedEventArgs e)
+        {
+            string inputFilePath = InputFilePathTextBox.Text;
+            bool removePunctuation = RemovePunctuationCheckBox.IsChecked ?? false;
+            int minWordLength = int.Parse(MinWordLengthTextBox.Text);
+
+            string text = File.ReadAllText(inputFilePath);
+            text = RemoveShortWords(text, minWordLength);
+            if (removePunctuation)
+            {
+                text = RemovePunctuation(text);
+            }
+
+            // Указываем путь для сохранения измененного текста
+            string outputFilePath = @"G:\MakarovLab1\Output.txt";
+            File.WriteAllText(outputFilePath, text);
+
+            MessageBox.Show("File processed successfully and saved to " + outputFilePath);
+        }
+
+        private string RemoveShortWords(string text, int minLength)
+        {
+            return string.Join(" ", text.Split(' ').Where(word => word.Length >= minLength));
+        }
+
+        private string RemovePunctuation(string text)
+        {
+            return Regex.Replace(text, @"[^\w\s]", "");
+        }
+
+        private void RemovePunctuationCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
